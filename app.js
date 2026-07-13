fa19a25810621bd8f185f6e3952774ae25ac07fb
ray83810 <ray83810@gmail.com>
2026-07-13 15:50:28 +0800

    fix: labels to correct Chinese, 9pt font, narrower date columns

--- app.js
+++ app.js
@@ -3434,8 +3434,8 @@ function exportRosterToExcel(targetStage = 1) {
   td, th {
     border: 1px solid #7F7F7F;
     text-align: center;
-    font-size: 10pt;
-    height: 22px;
+    font-size: 9pt;
+    height: 20px;
     vertical-align: middle;
     padding: 1px 3px;
   }
@@ -3449,8 +3449,8 @@ function exportRosterToExcel(targetStage = 1) {
   html += `  <tr>\n`;
   html += `    <th rowspan="2" class="hdr" style="width:110px;">${state.currentYear}</th>\n`;
   html += `    <th class="hdr" style="width:90px;">${MONTH_FULL}</th>\n`;
-  html += `    <th class="hdr" style="width:35px;">假</th>\n`;
-  html += `    <th rowspan="2" class="hdr" style="width:80px;">餐</th>\n`;
+  html += `    <th class="hdr" style="width:55px;">休假</th>\n`;
+  html += `    <th rowspan="2" class="hdr" style="width:65px;">用餐</th>\n`;
   html += `    <th rowspan="2" class="hdr" style="width:44px;">工作日</th>\n`;
   html += `    <th rowspan="2" class="hdr" style="width:44px;">OFF</th>\n`;
   html += `    <th rowspan="2" class="hdr" style="width:44px;">PTO</th>\n`;
@@ -3459,7 +3459,7 @@ function exportRosterToExcel(targetStage = 1) {
   for (let d = 1; d <= daysCount; d++) {
     const dow = getDayOfWeek(state.currentYear, state.currentMonth, d);
     const cs  = dow === 0 ? ' color:#FF0000;' : (dow === 6 ? ' color:#0070C0;' : '');
-    html += `    <th class="hdr" style="width:95px;${cs}">${weekdaysEng[dow]}</th>\n`;
+    html += `    <th class="hdr" style="width:80px;${cs}">${weekdaysEng[dow]}</th>\n`;
   }
   html += `  </tr>\n`;
 
